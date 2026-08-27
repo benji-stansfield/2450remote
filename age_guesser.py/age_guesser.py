34191a46ddb775415b6e9fdd0a660f6f34a569bb
benji-stansfield <D00448726@utahtech.edu>
2026-08-26 20:08:35 -0600

    working repo

--- age_guesser.py/age_guesser.py
+++ age_guesser.py/age_guesser.py
@@ -1,15 +1,18 @@
+import random
+
 def get_age():
     name = input("What is your name? ")
     result = 'n'
+
     while (result != 'y'):
-        rand = random.randrange(15, 41)
-        age_guess = rand
-        print(f"{rand}")
+        age_guess = random.randrange(15, 41)
+        print(age_guess)
         result = input("Is this your age? ('y' or 'n') ")
+
         if (result == 'y'):
             print(f"{name} is {age_guess} years old!")
             return
         else:
             print("Rats")
+
 get_age()
-    
